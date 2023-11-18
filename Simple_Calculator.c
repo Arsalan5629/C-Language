3abe79e0f18a3c4c24fca34a989ef6c21e5e1d37
Arsalan Ali <146450293+Arsalan5629@users.noreply.github.com>
2023-11-18 18:48:30 +0500

    Simple_Calculator
    This C program serves as a basic calculator that performs addition, subtraction, multiplication, and division based on user input. The program prompts the user to enter two floating-point numbers and an operator (+, -, *, or /). It then checks if the entered numbers are negative and terminates with an informative error message if they are.
    
    Additionally, the program checks for division by zero when the operator is '/', ensuring that the calculation is valid. If the user enters an invalid operator, the program terminates with an informative error message.

---
+++ Simple_Calculator.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2;
+    char op;
+
+    printf("Enter number 1: ");
+    scanf("%f", &num1);
+
+    // This if condition will check if number 1 is negative.
+    if (num1 < 0) {
+        printf("Informative error\n");
+        // If the number is invalid, the code will finish and show the message.
+        return -1;
+    }
+
+    printf("Enter number 2: ");
+    scanf("%f", &num2);
+
+    // This if condition will check if number 2 is negative.
+    if (num2 < 0) {
+        printf("Informative error\n");
+        return -1;
+    }
+
+    printf("Enter operator: ");
+    scanf(" %c", &op);
+
+    if (op == '+') {
+        printf("The sum of %.1f and %.1f will be = %.1f\n", num1, num2, num1 + num2);
+    } else if (op == '-') {
+        printf("The difference of %.1f and %.1f will be = %.1f\n", num1, num2, num1 - num2);
+    } else if (op == '*') {
+        printf("The product of %.1f and %.1f will be = %.1f\n", num1, num2, num1 * num2);
+    } else if (op == '/') {
+        // Check if the second number is zero to avoid division by zero.
+        if (num2 == 0) {
+            printf("Error: Division by zero is not allowed\n");
+            return -1;
+        }
+        printf("The division of %.1f and %.1f will be = %.1f\n", num1, num2, num1 / num2);
+    } else {
+        // If the operator is invalid, finish the code and show the message.
+        printf("Informative error\n");
+        return -1;
+    }
+
+    return 0;
+}
